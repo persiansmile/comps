60f70942378f6b7b87b5fb3dd860628ce6cc5c41
Davood <da.mirzaee@gmail.com>
2023-05-15 18:03:53 +0330

    table work without correcting its type error

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-collections-home',
   templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  styleUrls: ['./collections-home.component.css'],
 })
 export class CollectionsHomeComponent {
-
+  data!: [
+    { name: 'davood'; age: 14; job: 'modifier'; kare: 'Med' },
+    { name: 'amin'; age: 24; job: 'advisor'; kare: 'High' },
+    { name: 'javad'; age: 25; job: 'anihilator'; kare: 'Low' }
+  ];
+  headers!: [
+    { key: 'name'; label: 'Name' },
+    { key: 'kare'; label: 'Kare' },
+    { key: 'age'; label: 'Age' },
+    { key: 'job'; label: 'Job' }
+  ];
 }
---
+++ src/app/collections/table/table.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.css'],
+})
+export class TableComponent implements OnInit {
+  data!: [
+    { name: 'davood'; age: 14; job: 'modifier' },
+    { name: 'amin'; age: 24; job: 'advisor' },
+    { name: 'javad'; age: 25; job: 'anihilator' }
+  ];
+  headers!: [
+    { key: 'name'; label: 'Name' },
+    { key: 'age'; label: 'Age' },
+    { key: 'job'; label: 'Job' }
+  ];
+
+  constructor() {}
+  ngOnInit(): void {
+    this.data = [
+      { name: 'davood', age: 14, job: 'modifier' },
+      { name: 'amin', age: 24, job: 'advisor' },
+      { name: 'javad', age: 25, job: 'anihilator' },
+    ];
+    this.headers = [
+      { key: 'name', label: 'Name' },
+      { key: 'age', label: 'Age' },
+      { key: 'job', label: 'Job' },
+    ];
+  }
+}
